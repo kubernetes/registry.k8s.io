340c13dc01a9bd585cc5d54780634c67d0513ce3
Benjamin Elder <bentheelder@google.com>
2022-01-26 20:35:02 -0800

    Add Tim's simple redirector sketch, give it a name.
    αρχείο (archeío) is roughly? Greek for "registry".
    
    Source originally from https://github.com/kubernetes/k8s.io/issues/1834#issuecomment-949977061
    
    See also: https://docs.google.com/document/d/1yNQ7DaDE5LbDJf9ku82YtlKZK0tcg5Wpk9L72-x2S2k/
    
    Co-authored-by: Tim Hockin <thockin@google.com>

---
+++ cmd/archeio/main.go
@@ -0,0 +1,72 @@
+/*
+Copyright 2022 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"regexp"
+	"strings"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Printf("listening on port %s", port)
+	http.ListenAndServe(":"+port, http.HandlerFunc(handler))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+	switch {
+	case strings.HasPrefix(path, "/v2/"):
+		doV2(w, r)
+	case strings.HasPrefix(path, "/v1/"):
+		doV1(w, r)
+	default:
+		log.Printf("unknown request: %q", path)
+		http.NotFound(w, r)
+	}
+}
+
+var reBlob = regexp.MustCompile("^/v2/.*/blobs/sha256:[0-9a-f]{64}$")
+
+func doV2(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+
+	if reBlob.MatchString(path) {
+		// Blob requests are the fun ones.
+		log.Printf("v2 blob request: %q", path)
+		//FIXME: look up the best backend
+		http.Redirect(w, r, "https://public.ecr.aws"+path, http.StatusTemporaryRedirect)
+		return
+	}
+
+	// Anything else (manifests in particular) go to the canonical registry.
+	log.Printf("v2 request: %q", path)
+	http.Redirect(w, r, "https://index.docker.io"+path, http.StatusPermanentRedirect)
+}
+
+func doV1(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+	log.Printf("v1 request: %q", path)
+	//FIXME: look up backend?
+	http.Redirect(w, r, "https://index.docker.io"+path, http.StatusPermanentRedirect)
+}
