265d48abd6245cfefba2c745904412723fcc3838
Caleb Woodbine <calebwoodbine.public@gmail.com>
2022-07-11 09:51:45 +1200

    Update s3 urls for dualstack

--- cmd/archeio/app/buckets.go
+++ cmd/archeio/app/buckets.go
@@ -30,8 +30,9 @@ import (
 func awsRegionToS3URL(region string) string {
 	switch region {
 	case "us-west-1", "us-west-2", "us-east-1", "us-east-2", "eu-central-1", "ap-southeast-1", "ap-northeast-1", "ap-south-1":
-		return fmt.Sprintf("https://prod-registry-k8s-io-%v.s3.%v.amazonaws.com", region, region)
+		return fmt.Sprintf("https://prod-registry-k8s-io-%v.s3.dualstack.%v.amazonaws.com", region, region)
 	default:
+		// default to us-east-2 for stability purposes
 		return "https://prod-registry-k8s-io-us-east-2.s3.dualstack.us-east-2.amazonaws.com"
 	}
 }
