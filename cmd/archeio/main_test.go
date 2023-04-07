2aff3ed56a62d20a0ef5954cb3f79215d09e125b
Benjamin Elder <bentheelder@google.com>
2023-04-06 17:07:44 -0700

    rewrite integration test to fake coming from different routing locations

--- cmd/archeio/main_test.go
+++ cmd/archeio/main_test.go
@@ -20,15 +20,25 @@ limitations under the License.
 package main
 
 import (
-	"context"
+	"fmt"
 	"net/http"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"testing"
 	"time"
 
+	"github.com/google/go-containerregistry/cmd/crane/cmd"
+	"github.com/google/go-containerregistry/pkg/crane"
 	"k8s.io/registry.k8s.io/internal/integration"
 )
+
+type integrationTestCase struct {
+	Name   string
+	FakeIP string
+	Image  string
+	Digest string
+}
 
 // TestIntegrationMain tests the entire, built binary with an integration
 // test, pulling images with crane
@@ -37,10 +47,6 @@ func TestIntegrationMain(t *testing.T) {
 	rootDir, err := integration.ModuleRootDir()
 	if err != nil {
 		t.Fatalf("Failed to detect module root dir: %v", err)
-	}
-	// NOTE: also ensures rootDir/bin is in front of $PATH
-	if err := integration.EnsureCrane(rootDir); err != nil {
-		t.Fatalf("Failed to ensure crane: %v", err)
 	}
 
 	// build binary
@@ -54,15 +60,22 @@ func TestIntegrationMain(t *testing.T) {
 	testPort := "61337"
 	testAddr := "localhost:" + testPort
 	serverErrChan := make(chan error)
-	cmdContext, serverCancel := context.WithCancel(context.TODO())
-	serverCmd := exec.CommandContext(cmdContext, "archeio")
+	serverCmd := exec.Command("./archeio", "-v=9")
+	serverCmd.Dir = filepath.Join(rootDir, "bin")
 	serverCmd.Env = append(serverCmd.Env, "PORT="+testPort)
-	// serverCmd.Stderr = os.Stderr
-	defer serverCancel()
+	serverCmd.Stderr = os.Stderr
 	go func() {
 		serverErrChan <- serverCmd.Start()
 		serverErrChan <- serverCmd.Wait()
 	}()
+	t.Cleanup(func() {
+		if err := serverCmd.Process.Signal(os.Interrupt); err != nil {
+			t.Fatalf("failed to signal archeio: %v", err)
+		}
+		if err := <-serverErrChan; err != nil {
+			t.Fatalf("archeio did not exit cleanly: %v", err)
+		}
+	})
 
 	// wait for server to be up and running
 	startErr := <-serverErrChan
@@ -76,30 +89,105 @@ func TestIntegrationMain(t *testing.T) {
 		t.Fatal("timed out waiting for archeio to be ready")
 	}
 
-	// TODO: fake being on AWS
-	testPull := func(image string) {
-		// nolint:gosec // this is not user suplied input ...
-		cmd := exec.Command("crane", "pull", testAddr+"/"+image, os.DevNull)
-		//cmd.Stderr = os.Stderr
-		out, err := cmd.CombinedOutput()
-		if err != nil {
-			t.Errorf("pull for %q failed: %v", image, err)
-			t.Error("output: ")
-			t.Error(string(out))
-			t.Fail()
+	// perform many test pulls ...
+	testCases := makeTestCases()
+	for i := range testCases {
+		tc := testCases[i]
+		t.Run(tc.Name, func(t *testing.T) {
+			t.Parallel()
+			ref := testAddr + "/" + tc.Image
+			// ensure we supply fake IP info from test case
+			craneOpts := []crane.Option{crane.WithTransport(newFakeIPTransport(tc.FakeIP))}
+			// test fetching digest first
+			digest, err := crane.Digest(ref, craneOpts...)
+			if err != nil {
+				t.Errorf("Fetch digest for %q failed: %v", ref, err)
+			}
+			if digest != tc.Digest {
+				t.Errorf("Wrong digest for %q", ref)
+				t.Errorf("Received: %q", digest)
+				t.Errorf("Expected: %q", tc.Digest)
+			}
+			err = pull(ref, craneOpts...)
+			if err != nil {
+				t.Errorf("Pull for %q failed: %v", ref, err)
+			}
+		})
+	}
+}
+
+func makeTestCases() []integrationTestCase {
+	wellKnownImages := []struct {
+		Name   string
+		Digest string
+	}{
+		{
+			Name:   "pause:3.1",
+			Digest: "sha256:f78411e19d84a252e53bff71a4407a5686c46983a2c2eeed83929b888179acea",
+		},
+		{
+			Name:   "pause:3.9",
+			Digest: "sha256:7031c1b283388d2c2e09b57badb803c05ebed362dc88d84b480cc47f72a21097",
+		},
+	}
+
+	interestingIPs := []struct {
+		Name string
+		IP   string
+	}{
+		{
+			Name: "GCP",
+			IP:   "35.220.26.1",
+		},
+		{
+			Name: "AWS",
+			IP:   "35.180.1.1",
+		},
+		{
+			Name: "Definitely-External",
+			// we obviously won't see this in the wild, but we also know
+			// it should not match GCP, AWS or any future providers
+			IP: "192.168.0.1",
+		},
+	}
+
+	// generate testcases from test data
+	testCases := []integrationTestCase{}
+	for _, image := range wellKnownImages {
+		for _, ip := range interestingIPs {
+			testCases = append(testCases, integrationTestCase{
+				Name:   fmt.Sprintf("IP:%s (%q),Image:%q", ip.Name, ip.IP, image.Name),
+				FakeIP: ip.IP,
+				Image:  image.Name,
+				Digest: image.Digest,
+			})
 		}
 	}
+	return testCases
+}
 
-	// test pulling pause image
-	// TODO: test pulling more things
-	testPull("pause:3.1")
+func pull(image string, options ...crane.Option) error {
+	puller := cmd.NewCmdPull(&options)
+	puller.SetArgs([]string{image, "/dev/null"})
+	return puller.Execute()
+}
 
-	// we're done, cleanup
-	if err := serverCmd.Process.Signal(os.Interrupt); err != nil {
-		t.Fatalf("failed to signal archeio: %v", err)
-	}
-	if err := <-serverErrChan; err != nil {
-		t.Fatalf("archeio did not exit cleanly: %v", err)
+type fakeIPTransport struct {
+	fakeXForwardFor string
+	h               http.RoundTripper
+}
+
+var _ http.RoundTripper = &fakeIPTransport{}
+
+func (f *fakeIPTransport) RoundTrip(r *http.Request) (*http.Response, error) {
+	r.Header.Add("X-Forwarded-For", f.fakeXForwardFor)
+	return f.h.RoundTrip(r)
+}
+
+func newFakeIPTransport(fakeIP string) *fakeIPTransport {
+	return &fakeIPTransport{
+		fakeXForwardFor: fakeIP + ",0.0.0.0",
+		h:               http.DefaultTransport,
 	}
 }
 
