f9bb82709ae10ee6c31f62c7cbb252b975db7a6e
Benjamin Elder <bentheelder@google.com>
2023-05-09 22:22:37 -0700

    drop gov and GLOBAL regions
    if we somehow identify in coming traffic from these it makes more sense to fallback to the defaultURL in our current architecture

--- cmd/archeio/internal/app/buckets_test.go
+++ cmd/archeio/internal/app/buckets_test.go
@@ -23,7 +23,7 @@ import (
 func TestRegionToAWSRegionToHostURL(t *testing.T) {
 	// ensure known regions return a configured bucket
 	regions := []string{
-		"GLOBAL", "af-south-1", "ap-east-1",
+		"af-south-1", "ap-east-1",
 		"ap-northeast-1", "ap-northeast-2", "ap-northeast-3",
 		"ap-south-1", "ap-south-2", "ap-southeast-1",
 		"ap-southeast-2", "ap-southeast-3", "ap-southeast-4",
@@ -31,7 +31,7 @@ func TestRegionToAWSRegionToHostURL(t *testing.T) {
 		"cn-northwest-1", "eu-central-1", "eu-central-2", "eu-north-1",
 		"eu-south-1", "eu-south-2", "eu-west-1", "eu-west-2", "eu-west-3",
 		"il-central-1", "me-central-1", "me-south-1", "sa-east-1", "us-east-1",
-		"us-east-2", "us-gov-east-1", "us-gov-west-1", "us-west-1", "us-west-2",
+		"us-east-2", "us-west-1", "us-west-2",
 	}
 	for _, region := range regions {
 		url := awsRegionToHostURL(region, "")
